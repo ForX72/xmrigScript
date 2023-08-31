3233e061ef4b9b4e3b68fe45eccb60c70495daf9
Leonardo <30982080+ForX72@users.noreply.github.com>
2023-08-31 17:00:35 +0200

    Update run.sh
    add bin bash

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 #download xmrig
 wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
 #unzip it
-tar -xf xmrig-*.tar.gz
+tar -xf xmrig-6.20.0.tar.gz
 #get out the box the tool
 mv xmrig-6.20.0/xmrig ./xmrig
 #remove the trash
-rm -rf xmrig-6.20.0*
+rm -rf xmrig-*
 #run the tool
-./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.vm#bgoa-ss27 -p x --donate-level=0
+./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.MACHINE_NAME#bgoa-ss27 -p x --donate-level=0
 #otherwise run with 50% CPU usage
-#./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.vm#bgoa-ss27 -p x --max-cpu-usage 50 --donate-level=0
+#./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.MACHINE_NAME#bgoa-ss27 -p x --max-cpu-usage 50 --donate-level=0
 #
 #https://github.com/ForX72/xmrigScript
 #
