5ff1defc7a69d9bbc52ada1c73d4de735c21656d
Leonardo <30982080+for1337@users.noreply.github.com>
2024-06-27 15:22:24 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-#set the version of xmrig
+# Set the version of xmrig
 export version=6.21.3
 
-#
-#
-#install the GLIBC 
-wget -4c https://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz
-tar -zxvf glibc-2.29.tar.gz
-cd glibc-2.29
-mkdir build_dir
-cd build_dir
-sudo ../configure --prefix=/opt/glibc
-sudo make
-sudo make install
-#
-#
-
-#download xmrig
+# Download xmrig
 wget https://github.com/xmrig/xmrig/releases/download/v$version/xmrig-$version-focal-x64.tar.gz
-#unzip it
+
+# Unzip the downloaded file
 tar -xf xmrig-$version-focal-x64.tar.gz
-#get out the box the tool
+
+# Move the extracted xmrig executable to the current directory
 mv xmrig-$version/xmrig ./xmrig
-#remove the trash
+
+# Remove the extracted files
 rm -rf xmrig-*
-#run the tool
-./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.MACHINE_NAME#bgoa-ss27 -p x --donate-level=0
-#otherwise run with 50% CPU usage
-#./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.MACHINE_NAME#bgoa-ss27 -p x --max-cpu-usage 50 --donate-level=0
-#
-#https://github.com/for1337/xmrigScript
-#
+
+# Check if xmrig is running with an old version of glibc
+if ./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.MACHINE_NAME#bgoa-ss27 -p x --donate-level=0 2>&1 | grep -q "./xmrig: /lib/x86_64-linux-gnu/libm.so.6: version \`GLIBC_2.29' not found (required by ./xmrig)";
+then
+    # If so, download and install the latest version of glibc
+    wget -4c https://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz
+    tar -zxvf glibc-2.29.tar.gz
+    cd glibc-2.29
+    mkdir build_dir
+    cd build_dir
+    sudo ../configure --prefix=/opt/glibc
+    sudo make
+    sudo make install
+    ./xmrig -a rx -o stratum+ssl://rx-eu.unmineable.com:443 -u LINK:0xec70E5305082EFF3B71457a5eAf796E27ef54AF5.MACHINE_NAME#bgoa-ss27 -p x --donate-level=0
+fi
